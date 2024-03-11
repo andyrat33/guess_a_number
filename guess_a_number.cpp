3b3e2e2225d0c6d75af07e87250cadaa8e070a73
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2024-03-11 09:36:12 +0000

    changed logic structure to remote redundancy

--- guess_a_number.cpp
+++ guess_a_number.cpp
@@ -8,7 +8,7 @@ int main() {
 	int targetNumber = arc4random() % 1000 + 1;
 	int guessNumber = 0;
 
-	// cout << "The number is " << targetNumber << "\n"; 
+	cout << "The number is " << targetNumber << "\n"; 
 
 	cout << "Guess the number";
 	cout << "\n" << "I thought of a number between 1 and 1000\n";
@@ -23,10 +23,10 @@ int main() {
 		} else if (guess > targetNumber) 
 			{
 			cout << "\nLower\n";
-		} else if (guess == targetNumber) {
+		} else //if (guess == targetNumber) {
+		{
 			notGuessed = false;
 			cout << "Congratulations you got it!\n";
-			break;
 		}
 	}
 	
